747cd25113dcd4817a05a00eaa2b0eb03fd68390
Gregory Jefferis <jefferis@gmail.com>
2018-06-25 12:23:33 +0100

    Add citation

--- R/physplit.analysis.R
+++ R/physplit.analysis.R
@@ -1,8 +1,12 @@
-#' Packaged and versioned Analysis functions for Shahar's cells
+#' Packaged and versioned analysis functions for Shahar's cells
 #'
-#' these functions were originally part of RandIgor
+#' These functions were originally part of RandIgor.
 #'
 #' @docType package
+#' @references Shahar Frechter, Alexander S. Bates, Sina Tootoonian,
+#'   Michael-John Dolan, James D. Manton, Arian Jamasb, Johannes Kohl, Davi
+#'   Bock, Gregory S.X.E. Jefferis. bioRxiv 336982
+#'   \href{https://doi.org/10.1101/336982}{doi: 10.1101/336982}
 #' @name physplit.analysis
 #' @import gphys
 #' @useDynLib physplit.analysis
